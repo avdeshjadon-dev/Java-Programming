a384460e90143cf86b3cabd1530a3634f1795119
Avdesh Jadon <theavdeshjadon@gmail.com>
2025-08-12 11:13:25 +0530

    commit

---
+++ src/Array/TwoPointer.java
@@ -0,0 +1,21 @@
+package Array;
+
+import java.util.Arrays;
+
+public class TwoPointer {
+    public static void main(String[] args) {
+        int [] arr={1,0,0,1,0,1,1,0,0};
+//        for(int i=0;i<arr.length;i++){
+//            for(int j=i+1;j<arr.length;j++){
+//                if(arr[i]>arr[j]){
+//                    int temp=arr[i];
+//                    arr[i]=arr[j];
+//                    arr[j]=temp;
+//                }
+//            }
+//        }
+//        System.out.println(Arrays.toString(arr));
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
