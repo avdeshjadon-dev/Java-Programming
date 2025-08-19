ad58996eb6e5e8d68049711966e1b72042907e94
avdeshjadon <theavdeshjadon@gmail.com>
2025-08-19 12:30:36 +0530

    commit

--- src/Array/prefixSumArrayWithoutUsingExtraArray.java
+++ src/Array/prefixSumArrayWithoutUsingExtraArray.java
@@ -30,7 +30,6 @@ public class prefixSumArrayWithoutUsingExtraArray {
             arr[i] += arr[i - 1];
         }
     }
-
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
